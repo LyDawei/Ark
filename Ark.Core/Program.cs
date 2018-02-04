c58340839b1fd0a9f9c77df5018645d3122099db
David Ly <dly5891@gmail.com>
2018-02-03 23:50:58 -0600

    REnamed Ark.Business to Ark.Core

---
+++ Ark.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ark.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
