85d447015b9be2ba6108ce109cc29d67c5680476
David Ly <dly5891@gmail.com>
2018-02-18 16:24:28 -0600

    master: got the settings configured properly to upload directly to azure.

--- ark/manifest/models/animal_image.py
+++ ark/manifest/models/animal_image.py
@@ -4,4 +4,4 @@ from .animal import Animal
 
 class AnimalImage(models.Model):
     animal_id = models.ForeignKey(Animal)
-    image = models.FileField(upload_to='https://ark001.blob.core.windows.net/cats', blank=True)
+    image = models.FileField(blank=True)
